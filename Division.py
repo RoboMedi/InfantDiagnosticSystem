6cd25937d5895998b5816d3dc1124bfe949881c5
Sooyeol Lee <sooyeol.lee@ge.com>
2016-07-15 15:16:35 +0900

    Division

---
+++ Division.py
@@ -0,0 +1,13 @@
+class division:
+
+    def __init__(self):
+        return
+
+    def execute(self, a, b):
+        if b == 0:
+            print "Division by 0"
+            return 0
+        else:
+            return a/float(b)
+
+
